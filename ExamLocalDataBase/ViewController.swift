4aba6a330ca7c8247fa580560cef5e5d3ca19556
SnehKalariya <snehkalariya9583@gmail.com>
2023-04-12 11:07:58 +0530

    Initial Commit

---
+++ ExamLocalDataBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamLocalDataBase
+//
+//  Created by Sneh kalariya on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
